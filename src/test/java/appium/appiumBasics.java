9355727f1c02da63412a4e738cc9bbdf4e759940
ardra-ashok <ardraasok@yahoo.in>
2024-09-29 16:15:50 +1000

    added basetest class for saving configureAppium and tear down methods

--- src/test/java/appium/appiumBasics.java
+++ src/test/java/appium/appiumBasics.java
@@ -1,6 +1,9 @@
 package appium;
 
 import java.io.File;
+
+import io.appium.java_client.AppiumBy;
+import io.appium.java_client.AppiumDriver;
 import io.appium.java_client.android.AndroidDriver;
 import io.appium.java_client.service.local.AppiumDriverLocalService;
 import io.appium.java_client.android.options.UiAutomator2Options;
@@ -12,31 +15,12 @@ import java.net.URISyntaxException;
 import java.net.URL;
 
 
-public class appiumBasics {
+public class appiumBasics extends baseTest{
     @Test
     public void appiumTest() throws MalformedURLException, URISyntaxException {
-        File logDir = new File("/Users/aashok/IdeaProjects/appium_learnings/appium_logs");
-        if (!logDir.exists()) {
-            logDir.mkdirs();
-        }
-
-        File logFile = new File(logDir, "appium-logfile.log");
-
-        AppiumDriverLocalService service = new AppiumServiceBuilder()
-                .withAppiumJS(new File("/usr/local/lib/node_modules/appium/build/lib/main.js"))
-                .withIPAddress("127.0.0.1")
-                .withLogFile(logFile)
-                .usingPort(4723)
-                .build();
-        service.start();
-        UiAutomator2Options options = new UiAutomator2Options();
-        options.setDeviceName("Pixel_3a_testEmulator");
-        options.setApp("//Users//aashok//IdeaProjects//appium_learnings//src//test//resources//ApiDemos-debug.apk");
-//		AndroidDriver driver = new AndroidDriver(new URI("http://127.0.0.1:4723").toURL(),options);
-        AndroidDriver driver = new AndroidDriver(new URL("http://127.0.0.1:4723"),options);
-        driver.quit();
-        service.stop();
+        driver.findElement(AppiumBy.accessibilityId("Preference")).click();
     }
+
 }
 
 
---
+++ src/test/java/appium/baseTest.java
@@ -0,0 +1,52 @@
+package appium;
+
+import java.io.File;
+
+import io.appium.java_client.AppiumBy;
+import io.appium.java_client.AppiumDriver;
+import io.appium.java_client.android.AndroidDriver;
+import io.appium.java_client.service.local.AppiumDriverLocalService;
+import io.appium.java_client.android.options.UiAutomator2Options;
+import io.appium.java_client.service.local.AppiumServiceBuilder;
+import org.testng.annotations.AfterClass;
+import org.testng.annotations.BeforeClass;
+import org.testng.annotations.Test;
+import java.net.MalformedURLException;
+import java.net.URI;
+import java.net.URISyntaxException;
+import java.net.URL;
+
+
+public class baseTest {
+    AppiumDriverLocalService service;
+    AndroidDriver driver;
+    @BeforeClass
+    public void configureAppium() throws MalformedURLException {
+        File logDir = new File("/Users/aashok/IdeaProjects/appium_learnings/appium_logs");
+        if (!logDir.exists()) {
+            logDir.mkdirs();
+        }
+
+        File logFile = new File(logDir, "appium-logfile.log");
+
+        service = new AppiumServiceBuilder()
+                .withAppiumJS(new File("/usr/local/lib/node_modules/appium/build/lib/main.js"))
+                .withIPAddress("127.0.0.1")
+                .withLogFile(logFile)
+                .usingPort(4723)
+                .build();
+        service.start();
+        UiAutomator2Options options = new UiAutomator2Options();
+        options.setDeviceName("Pixel_3a_testEmulator");
+
+//      Xpath, id, accessibilityId, className , androidUiAutomator
+        options.setApp("//Users//aashok//IdeaProjects//appium_learnings//src//test//resources//ApiDemos-debug.apk");
+//		AndroidDriver driver = new AndroidDriver(new URI("http://127.0.0.1:4723").toURL(),options);
+        driver = new AndroidDriver(new URL("http://127.0.0.1:4723"),options);
+    }
+    @AfterClass
+    public void tearDown(){
+        driver.quit();
+        service.stop();
+    }
+}
