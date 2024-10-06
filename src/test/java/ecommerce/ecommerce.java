895726623da179b87a273d591629b6c6af4078b2
ardra-ashok <ardraasok@yahoo.in>
2024-10-06 11:00:29 +1100

    verified cart total

--- src/test/java/ecommerce/ecommerce.java
+++ src/test/java/ecommerce/ecommerce.java
@@ -3,12 +3,14 @@ package ecommerce;
 import appium.baseTest;
 import io.appium.java_client.AppiumBy;
 import org.openqa.selenium.By;
+import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.WebDriverWait;
 import org.testng.Assert;
 import org.testng.annotations.Test;
 
 import java.time.Duration;
+import java.util.List;
 
 public class ecommerce extends baseTest {
 
@@ -24,16 +26,40 @@ public class ecommerce extends baseTest {
 //        Assert.assertEquals(driver.findElement(By.xpath("(//android.widget.Toast)[1]")).getAttribute("name"),"Please enter your name");
         scrollToAnElement("Jordan 6 Rings");
         int productCount = driver.findElements(By.id("com.androidsample.generalstore:id/productName")).size();
-        for(int i=0;i<productCount;i++){
-            if(driver.findElements(By.id("com.androidsample.generalstore:id/productName")).get(i).getText().equals("Jordan 6 Rings"))
+        for (int i = 0; i < productCount; i++) {
+            if (driver.findElements(By.id("com.androidsample.generalstore:id/productName")).get(i).getText().equals("Jordan 6 Rings"))
                 driver.findElements(By.id("com.androidsample.generalstore:id/productAddCart")).get(i).click();
         }
         driver.findElement(By.id("com.androidsample.generalstore:id/appbar_btn_cart")).click();
-        Thread.sleep(3000);
 
         WebDriverWait wait = new WebDriverWait(driver, Duration.ofSeconds(5));
-        wait.until(ExpectedConditions.attributeContains(driver.findElement(By.id("com.androidsample.generalstore:id/toolbar_title")),"text","Cart"));
-        Assert.assertEquals(driver.findElement(By.id("com.androidsample.generalstore:id/productName")).getText(),"Jordan 6 Rings");
+        wait.until(ExpectedConditions.attributeContains(driver.findElement(By.id("com.androidsample.generalstore:id/toolbar_title")), "text", "Cart"));
+        Assert.assertEquals(driver.findElement(By.id("com.androidsample.generalstore:id/productName")).getText(), "Jordan 6 Rings");
+    }
+    @Test
+    public void verifyCartTotal() throws InterruptedException {
+        driver.findElement(By.xpath("//android.widget.EditText[@resource-id=\"com.androidsample.generalstore:id/nameField\"]")).sendKeys("Test Name");
+        driver.hideKeyboard();
+        driver.findElement(By.xpath("//android.widget.RadioButton[@text='Female']")).click();
+        driver.findElement(By.id("android:id/text1")).click();
+        scrollToAnElement("Argentina");
+        driver.findElement(By.xpath("//android.widget.TextView[@text=\"Antarctica\"]")).click();
+        driver.findElement(By.id("com.androidsample.generalstore:id/btnLetsShop")).click();
+        driver.findElements(By.xpath("//android.widget.TextView[@text='ADD TO CART']")).get(0).click();
+        driver.findElements(By.xpath("//android.widget.TextView[@text='ADD TO CART']")).get(0).click();
+        driver.findElement(By.id("com.androidsample.generalstore:id/appbar_btn_cart")).click();
+        Thread.sleep(2000);
+        WebDriverWait wait = new WebDriverWait(driver, Duration.ofSeconds(5));
+        wait.until(ExpectedConditions.attributeContains(driver.findElement(By.id("com.androidsample.generalstore:id/toolbar_title")), "text", "Cart"));
+
+        List<WebElement> productPrices = driver.findElements(By.id("com.androidsample.generalstore:id/productPrice"));
+        int productCount = productPrices.size();
+        double total = 0;
+        for(int i=0;i<productCount;i++){
+            total+= Double.parseDouble(productPrices.get(i).getText().replace("$",""));
+        }
+        double displaySum = Double.parseDouble(driver.findElement(By.id("com.androidsample.generalstore:id/totalAmountLbl")).getText().replace("$",""));
+        Assert.assertEquals(displaySum,total);
     }
 
 
