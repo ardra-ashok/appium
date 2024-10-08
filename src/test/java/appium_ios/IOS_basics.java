0306ddc834d3156f103b152862a3fbef3aa46858
ardra-ashok <ardraasok@yahoo.in>
2024-10-08 19:34:04 +1100

    Added predicate string selectors

--- src/test/java/appium_ios/IOS_basics.java
+++ src/test/java/appium_ios/IOS_basics.java
@@ -13,5 +13,11 @@ public class IOS_basics extends iosBaseTest {
         driver.findElement(AppiumBy.iOSClassChain("**/XCUIElementTypeStaticText[`label == 'Text Entry'`]")).click();
         driver.findElement(AppiumBy.iOSClassChain("**/XCUIElementTypeCell")).sendKeys("Hey there!");
         driver.findElement(AppiumBy.accessibilityId("OK")).click();
+//          // [c] both cases
+//        driver.findElement(AppiumBy.iOSNsPredicateString("type == 'XCUIElementTypeStaticText' AND value== 'Confirm / Cancel'")).click();
+//        driver.findElement(AppiumBy.iOSNsPredicateString("type == 'XCUIElementTypeStaticText' AND value BEGINSWITH[c] 'Confirm'")).click();
+        driver.findElement(AppiumBy.iOSNsPredicateString("type == 'XCUIElementTypeStaticText' AND value ENDSWITH[c] 'm / Cancel'")).click();
+        System.out.println(driver.findElement(AppiumBy.iOSNsPredicateString("name BEGINSWITH[c] 'A message'")).getText());
+        driver.findElement(AppiumBy.iOSNsPredicateString("label == 'Confirm'")).click();
     }
 }
